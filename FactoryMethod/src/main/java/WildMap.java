ab86caa1abd5f5bfb399373ca001bd295926ac4a
AE9180 <ae9180@student.jamk.fi>
2024-02-08 23:42:57 +0200

    wild map mod

--- FactoryMethod/src/main/java/WildMap.java
+++ FactoryMethod/src/main/java/WildMap.java
@@ -7,16 +7,12 @@ public class WildMap extends Map{
         Random rand = new Random();
         int randTile = rand.nextInt(3);
 
-        switch (randTile) {
-            case 0:
-                return new SwampTile();
-            case 1:
-                return new ForestTile();
-            case 2:
-                return new WaterTile();
-            default:
-                return null;
-        }
+        return switch (randTile) {
+            case 0 -> new SwampTile();
+            case 1 -> new ForestTile();
+            case 2 -> new WaterTile();
+            default -> null;
+        };
     }
 
     void display(){
