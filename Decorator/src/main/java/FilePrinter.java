0e87bc2a5ad7090a6a6c8d63f77d0408ee628650
siddarthsingotam <siddarthsingotam1213@gmail.com>
2024-03-15 02:41:31 +0200

    improved readability of file printer decorator class

--- Decorator/src/main/java/FilePrinter.java
+++ Decorator/src/main/java/FilePrinter.java
@@ -4,7 +4,7 @@ import java.io.File;
 
 public class FilePrinter extends PrinterDecorator{
     String FILENAME = "output.txt";
-    String FILE_PATH = "src/main/java";
+    String FILE_PATH = "Decorator/src/main/java";
     public FilePrinter(PrinterInterface printer) {
         super(printer);
     }
@@ -12,8 +12,12 @@ public class FilePrinter extends PrinterDecorator{
     @Override
     public void print(String message) {
         super.print(message);
-        String filepath = FILE_PATH + File.separator + FILENAME;
-        try (FileWriter writer = new FileWriter(filepath)){
+        recordMessageToFile(message);
+    }
+
+    private void recordMessageToFile(String message) {
+        String filepath2 = FILE_PATH + File.separator + FILENAME;
+        try (FileWriter writer = new FileWriter(filepath2)){
             writer.write(message);
             System.out.println("Successfully created the file in the module.");
         } catch (IOException e) {
@@ -21,15 +25,4 @@ public class FilePrinter extends PrinterDecorator{
             throw new RuntimeException(e);
         }
     }
-
-//    private void recordMessageToFile(String message) {
-//        String filepath2 = FILE_PATH + File.separator + FILENAME;
-//        try (FileWriter writer = new FileWriter(filepath2)){
-//            writer.write(message);
-//            System.out.println("Successfully created the file in the module.");
-//        } catch (IOException e) {
-//            System.out.println("Error writing to file: " + e.getMessage());
-//            throw new RuntimeException(e);
-//        }
-//    }
 }
